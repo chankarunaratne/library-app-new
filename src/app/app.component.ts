5e5ad454a9df02eaf5a3a4c3628ad20433a91b76
Chandima Karunaratne <c.karunaratne.mudiyanselage.10@student.scu.edu.au>
2024-09-29 18:27:57 +1000

    added search

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,17 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { BookListComponent } from './book-list/book-list.component';
 import { AddBookComponent } from './add-book/add-book.component';
+import { SearchBookComponent } from './search-book/search-book.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, BookListComponent, AddBookComponent],
+  imports: [
+    RouterOutlet,
+    BookListComponent,
+    AddBookComponent,
+    SearchBookComponent,
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
---
+++ src/app/search-book/search-book.component.ts
@@ -0,0 +1,29 @@
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { BookService } from '../services/book.service';
+import { Book } from '../book-list/book-list.component';
+
+@Component({
+  selector: 'app-search-book',
+  standalone: true,
+  imports: [FormsModule, CommonModule],
+  templateUrl: './search-book.component.html',
+  styleUrls: ['./search-book.component.css'],
+})
+export class SearchBookComponent {
+  searchQuery: string = '';
+  foundBook: Book | null = null;
+  searchPerformed: boolean = false;
+
+  constructor(private bookService: BookService) {}
+
+  searchBook() {
+    this.searchPerformed = true;
+    const books = this.bookService.getBooks();
+    this.foundBook =
+      books.find((book) =>
+        book.name.toLowerCase().includes(this.searchQuery.toLowerCase())
+      ) || null;
+  }
+}
